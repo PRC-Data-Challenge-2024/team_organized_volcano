1b65b405300cd0bb8f0442d54aba9f018a1d48c0
TheRealAkumetsu <cordtsmalte@googlemail.com>
2024-08-23 14:31:02 +0200

    Testing submissions, updated workflow to remove tmp file

--- submit_solution.py
+++ submit_solution.py
@@ -1,18 +1,21 @@
 import json
 import subprocess
 import os
-num = 0
+num = 1
 
 path_to_infos = "response_1723016820162.json"
 with open(path_to_infos) as fh:
     data = json.load(fh)
 
-print(data)
 team_id = data["team_id"]
 team_name = data["team_name"]
-sub_file = team_name + "_v" + num + "_" + team_id + ".csv"
-command = f"mc cp submission.csv dc24/submission/{sub_file}"
-if not os.path.exists(sub_file):
+sub_file = team_name + "_v" + str(num) + "_" + team_id + ".csv"
+
+command = f"mc alias set dc24 https://s3.opensky-network.org/ ZG58zJvKhts2bkOX eU95azmBpK82kg96mE0TNzsWov3OvP2d " \
+          f"&& mc alias list " \
+          f"&&  mc cp submission.csv dc24/submission/{sub_file}"
+
+if not os.path.exists("submission.csv"):
     raise Exception("Submission file does not exist, please save the submission as "
                     "'submission.csv' into this folder and try again :)")
 else:
---
+++ test_submission.py
@@ -0,0 +1,27 @@
+# Imports required packages and functions
+from pyopensky.s3 import S3Client
+from collections import Counter
+import pandas as pd
+import numpy as np
+import os
+# import plotly.express as px
+
+print("Hello")
+
+# Downloads the challenge_set.csv file containing the flightlist from the competition S3 bucket
+s3 = S3Client()
+
+if not os.path.exists("submission_set.csv"):
+    for obj in s3.s3client.list_objects("competition-data", recursive=True): # iterates over all objects in "competition-data"
+        if not obj.object_name.endswith("parquet"): # as "competition-data" contains .parquet and .csv files, only the latter are downloaded
+            s3.download_object(obj) # downloads object in "competition-data"
+
+challenge_df = pd.read_csv('challenge_set.csv')
+submission_df = pd.read_csv('submission_set.csv')
+
+print(submission_df.head())
+
+submission_df["tow"] = 100000
+submission_df = submission_df[["flight_id", "tow"]]
+submission_df.to_csv("submission.csv", index=False)
+
